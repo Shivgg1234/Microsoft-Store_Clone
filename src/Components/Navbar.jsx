9912eaa3ea6022d06849cd418a3c05ba078ed0c9
Yash Panpaliya <yashpanpaliya11@Yashs-MacBook-Pro-7.local>
2026-04-22 00:49:36 +0530

    all updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,10 +2,32 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav style={{ padding: "10px", background: "#111", color: "white" }}>
-      <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
-      <Link to="/apps" style={{ marginRight: "10px" }}>Apps</Link>
-      <Link to="/games">Games</Link>
+    <nav className="navbar">
+      <div className="nav-left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg"
+          alt="logo"
+          className="logo"
+        />
+        <span className="store-text"> Microsoft Store |</span>
+
+        <Link to="/">Home</Link>
+        <Link to="/apps">Apps</Link>
+        <Link to="/games">Games</Link>
+        <Link to="/about">About</Link>
+      </div>
+
+     
+      <div className="nav-center">
+        <input
+          type="text"
+          placeholder="Search apps, games, and more"/>
+      </div>
+
+      <div className="nav-right">
+        <button className="signbtn">Sign In</button>
+      </div>
+
     </nav>
   );
 }
