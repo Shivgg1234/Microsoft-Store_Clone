ea325915179c3c0aa29dc1b17f1de8e350bd8f7c
Yash Panpaliya <yashpanpaliya11@Yashs-MacBook-Pro-7.local>
2026-04-24 20:45:37 +0530

    optimized hero.jsx and hero.css

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,11 +1,11 @@
 import "./Hero.css";
 
-export default function Hero() {
+export default function Hero(prop) {
   return (
     <div className="container">
 
       <div className="hero">
-        <img src="https://wallpapers.com/images/hd/gta-5-landscape-art-2a58lbxzfpa0xjg7.jpg" alt="hero" />
+        <img src={prop.link1} alt="hero" />
 
         <div className="overlay">
           <h1>The Grand Theft Auto V</h1>
@@ -21,7 +21,7 @@ export default function Hero() {
 
 
         <div className="card large">
-          <img src="https://images-eds-ssl.xboxlive.com/image?url=7flt5HU26ZSS3Tgted_TMty0wzqMQYpm03yD7eAPRtQBYO5dMlD18uZxNDuKXvpqkJpgh0RHhtdzIqyPKZW.m.v1AImAOEpz1OL99rN2wAYgBqo8Z8eg3pfxHYVXOBbQIqJ_rl9QbRVAmKICe6.0XL2h7cm7j8ArPQM7wHpP3NJxL7Nnr5x07434qzOdXqSlhLiX6Y.9XdbMi9IIApZdyXYIZcp1EHKjDUjUjE6VPeSm0qTZlJUlv_jil3FfkehK&h=2160" alt="" />
+          <img src={prop.link2} alt="" />
           <div className="card-overlay">
             <h2>Image Lab in ChatOn</h2>
             <button>Get</button>
@@ -31,14 +31,14 @@ export default function Hero() {
   
         <div className="bottom">
           <div className="card small">
-            <img src="https://images-eds-ssl.xboxlive.com/image?url=7flt5HU26ZSS3Tgted_TMty0wzqMQYpm03yD7eAPRtQBYO5dMlD18uZxNDuKXvpqVIbEZOZfakQUY7fDpGSa3e1Gykn8Pp1YtZkzFxEhYar94NBAE036oMzRbPdlvvL2CMhyP6S7gLm7Ic9hmuBLImAPnLZM92fXV_J_7zjXrp..e_8HtPv1xmF7bSgbxAUUR4XegKXtkW2fk8U19g6f4lCuPfqto4aWYJhWvvU.EUE-&h=2160" alt="" />
+            <img src={prop.link3} alt="" />
             <div className="card-overlay">
               <p>New P&ID System Symbols</p>
             </div>
           </div>
 
           <div className="card small">
-            <img src="https://images-eds-ssl.xboxlive.com/image?url=7flt5HU26ZSS3Tgted_TMty0wzqMQYpm03yD7eAPRtQBYO5dMlD18uZxNDuKXvpqg3HOLmZashdzSWScCMmFSv5MKsebwjtqzRqDPo9r.rnTRstkanzfx9DvO8UoUvOfkc1m3q8Dfqj7J_Q05H0aJwK7.g8SPT1FhWKbRS_FO6FUaOfCThFZaHeow.IfALrMAgUZTU1ZZHijbx83H8bKjg--&h=2160" alt="" />
+            <img src={prop.link4} alt="" />
             <div className="card-overlay">
               <p>Easy Invoice Generator</p>
             </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,10 @@ import Hero from "../Components/Hero";
 export default function Home() {
   return (
     <div>
-      <Hero />
+      <Hero link1="https://images.hindustantimes.com/tech/img/2024/06/21/1600x900/GTA_5_1718973871744_1718973872239.jpg"
+             link2="https://images-eds-ssl.xboxlive.com/image?url=7flt5HU26ZSS3Tgted_TMty0wzqMQYpm03yD7eAPRtQBYO5dMlD18uZxNDuKXvpqVceA0dzaIgDzEmZ5MbZ8d3CcasIYhngoiq85LYF9jmkZL7emjpgu9q6YA2846crYfNpYVnodOL_65QY5MEoLn5YUIexiGNlf.GRU2aPHOd8rXaXz4TnWe5.qRfvH21KGf09P1IXylVyA3JFzoU1zJA--&h=2160"
+             link3="https://images-eds-ssl.xboxlive.com/image?url=7flt5HU26ZSS3Tgted_TMty0wzqMQYpm03yD7eAPRtQBYO5dMlD18uZxNDuKXvpqM3ncfGr6.0nmlI5PKktvnGRpvEcn1hOdydFMbCBHc6rilqg6mpYFG4yvueG.FlxPPNRaypJMfN9r1ANL_Hu9Kcm59e9Vc6XzThhQEntJidROHbawb_cdm9QTSEpz67PpgduXhLq5KA7VI78.AFpskbFFaQ8w_.lj_eZIweX6_3s-&h=2160"
+             link4="https://images-eds-ssl.xboxlive.com/image?url=7flt5HU26ZSS3Tgted_TMty0wzqMQYpm03yD7eAPRtQBYO5dMlD18uZxNDuKXvpq07Ih7oFbRKKOE.ITZGY6MjrMZtkfQqj.G90ewFki0ahYrate0_Sj0pwphAUYj7ldwIbuT8zzpKvFmNElZJbbLc2sRf07d8_ObTbV_kb_BmDUI2HxQPdOBljWZ2PQujy6dgPTAIi7mwv9UekZOE2aLA--&h=2160"/>
     </div>
   );
 }
