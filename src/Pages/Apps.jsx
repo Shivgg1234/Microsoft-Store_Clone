780c0bdfea3a99e4ac68d9c25c81830065b4453f
Yash Panpaliya <yashpanpaliya11@Yashs-MacBook-Pro-7.local>
2026-04-25 21:07:40 +0530

    changed bg color

--- src/Pages/Apps.jsx
+++ src/Pages/Apps.jsx
@@ -4,7 +4,8 @@ import ScrollSection from "../Components/ScrollSection";
 
 export default function Apps() {
   return (
-    <div>
+    <div data-theme="dark" style={{ backgroundColor: '#000000' }}>
+
       <Hero
         link1="https://images-eds-ssl.xboxlive.com/image?url=4rt9.lXDC4H_93laV1_eHM0OYfiFeMI2p9MWie0CvL99U4GA1gf6_kayTt_kBblFwHwo8BW8JXlqfnYxKPmmBcXm6WqCy1Yzd43klSX2PfqeV.suPzi_.a.lad5HNsZt8GOjlH843tLG0pWy8RWVb_zcLe4jEWnuve7wABPqHtE-&format=source&w=170"
         link2="https://images-eds-ssl.xboxlive.com/image?url=7flt5HU26ZSS3Tgted_TMty0wzqMQYpm03yD7eAPRtQBYO5dMlD18uZxNDuKXvpqQCi00OjlLOPIu.iaIAqkKDd2lnPYvKM79BnhiJV81NRD9XY6QpMbgmc20Oyka.d1RnAQ8o0Sv_J4FhgNWPhmg078vbw0tTUkJtkdLLvM1Io0e0i3KZV4FYRNIw.RjySeQK94t0VF7Y_px4RLxRGXSA--&h=2160"
@@ -13,10 +14,10 @@ export default function Apps() {
         text1="Top Apps Collection"
       />
 
-      {/* Sections */}
       <ScrollSection title="Popular Apps" data={data.apps.slice(0, 15)} />
       <ScrollSection title="Top Rated Apps" data={data.apps.slice(15, 30)} />
       <ScrollSection title="Productivity Apps" data={data.apps.slice(30, 50)} />
+
     </div>
   );
 }
